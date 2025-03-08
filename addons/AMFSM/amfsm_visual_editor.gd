017271f62e66e4da9216974a91145c7dda88e005
John Powell <john.a.powell@gmail.com>
2025-03-08 18:27:36 -0500

    Added basic zoom capabilities

--- addons/AMFSM/amfsm_visual_editor.gd
+++ addons/AMFSM/amfsm_visual_editor.gd
@@ -1,9 +1,68 @@
 @tool
-extends SubViewportContainer
+extends MarginContainer
+class_name AmfsmVisualEditor
 
-@onready var subview := $SubViewport
+const _ZOOM: PackedFloat32Array = [
+	0.0625, 0.125, 0.25, 1.0/3.0, 0.5, 2.0/3.0, 0.75, 0.875, 1.0, # up to 100%
+	1.5,    2.0,   2.5,  3.0,     4.0, 5.0,     6.0,  7.0,   8.0 # up to 800%
+]
+
+var by_name := {}
+var verts := {}
+var _host: EditorPlugin = null
+var zoom_idx: int = _ZOOM.bsearch(1.0)
+
+@onready var _grid: Panel = $Grid
+@onready var _ui: MarginContainer = $UI
+@onready var _zoom_in: Button = $UI/VBox/Controls/In
+@onready var _zoom_out: Button = $UI/VBox/Controls/Out
+@onready var _zoom_amount: Label = $UI/VBox/Controls/Amount
 
 
-func _on_resized() -> void:
-	prints("Resized", size)
-	#($SubViewport if subview == null else subview).size = size
+func _ready() -> void:
+	_grid.zoom = _ZOOM[zoom_idx]
+
+
+func set_plugin_host(host: EditorPlugin) -> void:
+	_host = host
+
+
+func set_states(_by_name: Dictionary) -> void:
+	by_name = _by_name
+
+
+func update_states(new_order: Array[StringName]) -> void:
+	prints(new_order)
+
+
+func _on_center_view() -> void:
+	_grid.grid_offset = Vector2.ZERO
+
+
+func _on_zoom_in() -> void:
+	var last := _ZOOM.size() - 1
+	var next := zoom_idx + 1
+	if next <= last:
+		_zoom_out.disabled = false
+		_zoom_in.disabled = next == last
+		zoom_idx = next
+		_grid.zoom = _ZOOM[zoom_idx]
+		_zoom_amount.text = "%5.1f%%" % (_ZOOM[zoom_idx] * 100)
+
+
+func _on_reset_zoom() -> void:
+	_zoom_in.disabled = false
+	_zoom_out.disabled = false
+	zoom_idx = _ZOOM.bsearch(1.0)
+	_grid.zoom = 1.0
+	_zoom_amount.text = "%5.1f%%" % (_ZOOM[zoom_idx] * 100)
+
+
+func _on_zoom_out() -> void:
+	var next := zoom_idx - 1
+	if next >= 0:
+		_zoom_in.disabled = false
+		_zoom_out.disabled = next == 0
+		zoom_idx = next
+		_grid.zoom = _ZOOM[zoom_idx]
+		_zoom_amount.text = "%5.1f%%" % (_ZOOM[zoom_idx] * 100)
--- addons/AMFSM/amfsm_visual_editor_grid.gd
+++ addons/AMFSM/amfsm_visual_editor_grid.gd
@@ -1,40 +1,75 @@
 @tool
-extends Node2D
+extends Panel
 
+@export_range(0.0625, 8, 0.0625) var zoom := 1.0:
+	set(val):
+		zoom = val
+		queue_redraw()
+		if center != null:
+			center.position = grid_offset * zoom + size * 0.5
 
-@onready var camera := $"../Camera2D"
 @export_group("Grid Settings", "grid_")
-@export var grid_draw_lines := true
-@export var grid_color := Color.SKY_BLUE
-@export var grid_draw_background := true
-@export var grid_background := Color.DARK_SLATE_GRAY
-@export var grid_spacing := 64
-@export var grid_width := 1
+@export var grid_draw_lines := true:
+	set(val):
+		grid_draw_lines = val
+		queue_redraw()
+@export var grid_color := Color.SKY_BLUE:
+	set(val):
+		grid_color = val
+		queue_redraw()
+@export var grid_draw_background := true:
+	set(val):
+		grid_draw_background = val
+		queue_redraw()
+@export var grid_background := Color.DARK_SLATE_GRAY:
+	set(val):
+		grid_background = val
+		queue_redraw()
+@export_range(4, 512, 1) var grid_spacing := 64:
+	set(val):
+		grid_spacing = val
+		queue_redraw()
+@export_range(1, 16, 1) var grid_width := 1:
+	set(val):
+		grid_width = val
+		queue_redraw()
+@export var grid_offset := Vector2.ZERO:
+	set(val):
+		grid_offset = val
+		queue_redraw()
+		if center != null:
+			center.position = grid_offset * zoom + size * 0.5
 
 var grid_lines := PackedVector2Array()
 
+@onready var center: Node2D = $Center
 
-# Called when the node enters the scene tree for the first time.
+
+
+
 func _draw() -> void:
-	var view := get_viewport_rect()
-	var size = view.size * camera.zoom / 2
-
 	if grid_draw_background:
-		view.position -= size
-		draw_rect(view, grid_background)
+		draw_rect(Rect2(Vector2.ZERO, self.size), grid_background)
 
 	if grid_draw_lines:
-		var cam = camera.position
-		var start := Vector2i(int((cam.x - size.x) / grid_spacing) - 1, int((cam.y - size.y) / grid_spacing) - 1)
-		var end := Vector2i(int((size.x + cam.x) / grid_spacing) + 1, int((size.y + cam.y) / grid_spacing) + 1)
-		grid_lines.clear()
-		grid_lines.resize(((end.x - start.x) + (end.y - start.y)) * 2)
+		var spacing = grid_spacing * zoom
+		var cam = size * -0.5 - grid_offset * zoom
+		cam.x = fmod(cam.x, spacing)
+		cam.y = fmod(cam.y, spacing)
 
-		for i in range(start.x, end.x):
-			grid_lines.append(Vector2(i * grid_spacing, cam.y + size.y))
-			grid_lines.append(Vector2(i * grid_spacing, cam.y - size.y))
-		for i in range(start.y, end.y):
-			grid_lines.append(Vector2(cam.x + size.x, i * grid_spacing))
-			grid_lines.append(Vector2(cam.x - size.x, i * grid_spacing))
+		var count := Vector2i(int(size.x / spacing) + 1, int(size.y / spacing) + 1)
+		grid_lines.clear()
+		grid_lines.resize((count.x + count.y) * 2)
+
+		for i in range(count.x):
+			grid_lines.append(Vector2(i * spacing - cam.x, 0.0   ))
+			grid_lines.append(Vector2(i * spacing - cam.x, size.y))
+		for i in range(count.y):
+			grid_lines.append(Vector2(0.0,    i * spacing - cam.y))
+			grid_lines.append(Vector2(size.x, i * spacing - cam.y))
 
 		draw_multiline(grid_lines, grid_color, grid_width)
+
+
+func _on_resized() -> void:
+	center.position = grid_offset + size * 0.5
